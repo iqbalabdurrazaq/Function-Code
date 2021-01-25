fc9960d9b99594ba46ef02fe02368a221886a20e
iqbal abdurrazaq <mancunianmerchant816@gmail.com>
2021-01-25 09:46:23 +0700

    comment not fix

---
+++ appsfreshbox - flutter/sidebarpage.dart
@@ -0,0 +1,39 @@
+
+          // body: isLoading
+          //     ? _role == "Pickers"
+          //         ? listPicker[index]
+          //         : _role == "QC B2C" || _role == "QC B2B"
+          //             ? listQc[index]
+          //             : Center(
+          //                 child: CircularProgressIndicator(),
+          //               )
+          //     : _role == "Procurement"
+          //         ? listProcurement[index]
+          //         : _role == "Receiving Staff"
+          //             ? listReceiving[index]
+          //             // : listHrm[index],
+          //             : Center(
+          //                 child: CircularProgressIndicator(),
+          //               ),
+          
+          
+          // if (_role == "Pickers")
+              //   listPicker[index]
+              // else if (_role == "QC B2C" || _role == "QC B2B")
+              //   listQc[index]
+              // else if (_role == "Procurement")
+              //   listProcurement[index]
+              // else if (_role == "Receiving Staff")
+              //   listReceiving[index]
+              // else
+              //   listHrm[index]
+              
+              
+              // if (_role != null) {
+    //   if (_role == "Pickers" || _role == "QC B2C" || _role == "QC B2B") {
+    //     if (mounted) {
+    //       _getPickerorder();
+    //       _getRejectorder();
+    //     }
+    //   }
+    // }
